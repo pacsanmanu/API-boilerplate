09a0e74bb2b1920c647b4d354c309af57939b0b8
Manuel Pacheco <mpdevlp@gmail.com>
2024-01-24 11:57:53 +0100

    Forgot to delete some imports :-1:

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,4 @@
 import express from 'express';
-import resendController from '../controllers/resend-controller.js';
-import { login } from '../controllers/users-controller.js'
-import { mailController } from '../controllers/mail-controller.js'
-
 
 const router = express.Router();
 
